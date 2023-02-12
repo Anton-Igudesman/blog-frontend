db0db93a1bb70fb9758d16c75c6d2a8028382ea7
Anton-Igudesman <antonguy@gmail.com>
2023-02-12 07:53:34 -0800

    changes

--- src/redux/slices/users/userSlice.js
+++ src/redux/slices/users/userSlice.js
@@ -1,7 +1,6 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { baseUrl } from '../../../utilities/baseUrl';
-import { Navigate } from 'react-router-dom';
 
 //register action
 
